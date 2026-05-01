feb81588e5731c790529649ea655ee02d00ea946
jiaul.islam.jibon <jiaulislam.ict.bd@gmail.com>
2026-05-01 16:41:24 +0600

    updating cors

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -100,8 +100,8 @@ REST_AUTH = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=15),
-    "REFRESH_TOKEN_LIFETIME": timedelta(seconds=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=3),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
 }
@@ -222,3 +222,5 @@ ADMIN_USER_LANGUAGE_SWITCHER = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS", "").split(",")
