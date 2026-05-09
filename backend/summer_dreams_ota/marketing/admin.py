c3980590056e61e3c4ed3063aa6ba87a39ea5c43
jiaul.islam.jibon <jiaulislam.ict.bd@gmail.com>
2026-05-10 00:07:20 +0600

    feat(marketing): make ContactInquiry read-only in Admin

--- backend/summer_dreams_ota/marketing/admin.py
+++ backend/summer_dreams_ota/marketing/admin.py
@@ -92,3 +92,7 @@ class ContactInquiryAdmin(admin.ModelAdmin):
     def has_add_permission(self, _request):
         # Inquiries should only be created via the API
         return False
+
+    def has_change_permission(self, _request, _obj=None):
+        # Inquiries should be read-only
+        return False
