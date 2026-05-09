fca90061adb271f34b23d45897ae85d4e71e2e5f
jiaul.islam.jibon <jiaulislam.ict.bd@gmail.com>
2026-05-09 15:27:57 +0600

    feat: install and integrate tanstack query devtools

--- frontend/src/providers/query-provider.tsx
+++ frontend/src/providers/query-provider.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactNode, useState } from 'react';
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
@@ -16,6 +17,9 @@ export default function QueryProvider({ children }: { children: ReactNode }) {
   );
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
