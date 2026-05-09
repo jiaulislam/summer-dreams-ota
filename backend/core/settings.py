91900c86ad3f6fb53556f12449bd6031a850d99f
jiaul.islam.jibon <jiaulislam.ict.bd@gmail.com>
2026-05-09 19:00:37 +0600

    feat(ops): implement CI/CD pipeline and docker infrastructure

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -265,6 +265,7 @@ ADMIN_USER_LANGUAGE_SWITCHER = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
